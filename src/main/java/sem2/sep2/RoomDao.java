b8d8f13d7c45047b6c45975187747692f853713f
321978557 <321978557@qq.com>
2024-05-03 00:35:49 +0200

    Update RoomDao.java

--- src/main/java/sem2/sep2/RoomDao.java
+++ src/main/java/sem2/sep2/RoomDao.java
@@ -1,8 +1,24 @@
 package sem2.sep2;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class RoomDao
 {
   public static Connection connection;
+  public RoomDao(Connection connection) {
+    this.connection = connection;
+  }
+
+  public void createRoom(String username, String password) throws SQLException
+  {
+    String sql = "INSERT INTO rooms (username, password) VALUES (?, ?)";
+    try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setString(1, username);
+      preparedStatement.setString(2, password);
+      preparedStatement.executeUpdate();
+    }
+  }
+
 }
